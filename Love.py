e1ff2985f6c3740f091f73929a41040cae40cce9
kahvahaajnn <Laudahab6362@outlook.com>
2025-02-02 08:11:50 +0530

    Update Love.py

--- Love.py
+++ Love.py
@@ -108,7 +108,7 @@ async def run_attack(chat_id, ip, port, time, context):
 
     try:
         process = await asyncio.create_subprocess_shell(
-            f"./Alone {ip} {port} {time} 500",
+            f"./Smokie {ip} {port} {time} 500",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
