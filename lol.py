45a997567f21040a085ac58202705b3e418658e9
kahvahaajnn <Laudahab6362@outlook.com>
2025-02-04 21:29:48 +0530

    Update lol.py

--- lol.py
+++ lol.py
@@ -4,10 +4,10 @@ from telegram.ext import Application, CommandHandler, CallbackContext
 import os
 
 # Configuration
-TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN"  # Fetch token from environment variable
+TELEGRAM_BOT_TOKEN = "7819992909:AAHfbmckp2vxVRCmu9hhFq42q_hWZfvu1HM"  # Fetch token from environment variable
 ADMIN_USER_ID = 1662672529
 APPROVED_IDS_FILE = 'approved_ids.txt'
-CHANNEL_ID = "@gosjsisnsnsnsjan"  # Replace with your channel username
+CHANNEL_ID = "@jsbananannanan"  # Replace with your channel username
 ATTACK_IN_PROGRESS = False
 
 REFERRAL_FILE = 'referrals.txt'  # File to track referrals
